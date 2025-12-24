cf80edb3f9c3a4ac0c5ce6833cdb68c2fe5da5ba
Gdmineofc <manulusacc@gmail.com>
2025-12-24 21:23:10 +0530

    Create le.js

---
+++ le.js
@@ -0,0 +1,7 @@
+2025-12-24T15:50:16.726154+00:00 heroku[web.1]: Process running mem=1643M(320.9%)
+2025-12-24T15:50:16.728669+00:00 heroku[web.1]: Error R15 (Memory quota vastly exceeded)
+2025-12-24T15:50:16.730969+00:00 heroku[web.1]: Stopping process with SIGKILL
+2025-12-24T15:50:16.886566+00:00 heroku[web.1]: Process exited with status 137
+2025-12-24T15:50:16.911609+00:00 heroku[web.1]: State changed from up to crashed
+2025-12-24T15:50:16.915866+00:00 heroku[web.1]: State changed from crashed to starting
+2025-12-24T15:50:31.631997+00:00 heroku[web.1]: Starting process with command `npm start`
